ec53e7f2c13ae83cba5b387e5d3258ac8238d0e5
Alkim <alkim-atanasov@outlook.com>
2023-08-25 21:43:29 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // Iteration 1: Names and Input
 
-
+let hacker1 = "Alkim";
+console.log(`the driver`s name is ${ hacker1 }`)
+let hacker2 = "Luis";
+console.log(´the navigator´s name  is $ {hacker2}`)
 // Iteration 2: Conditionals
+if (hacker1.length > hacker2 - length)
+{console.log(`the driver has the longest name,it has${hacker2.length}characters.`)
+    
+}
+
+else if (hacker2.length > hacker1.length)
+
+{
+    console.log(ìt seems that the navigator has the longest name,it has ${hacker1.length}
+    
+    )characters.`)}
+else(hacker2.length===hacker1.length)
+
+{console.log(`window,you both have equally long names, ${hacker1.length}characters!``)
 
 
 // Iteration 3: Loops
+    let driverName = ""
+    for (i = 0; i < hacker1.length; i++){
+        driverName += hacker1[i].toUpperCase() + "";
+    }
+    console.log(driverName)
